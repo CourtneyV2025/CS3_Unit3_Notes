e7842609254eda1cdf0a84268bac5e99700c47fc
CourtneyV2025 <courtney.valis2025@gbwl.org>
2025-01-22 15:41:43 +0000

    created lotssssssssssssss of html type

--- app.py
+++ app.py
@@ -12,6 +12,22 @@ def index():
 @app.route('/hello/<name_data>')
 def hello_there(name_data = None):
     return render_template('hello_there.html', name=name_data)
-    
+
+@app.route("/api/data")
+def get_data():
+    # displays data file returned from an API call
+    # can do more Python work to format this later
+    return app.send_static_file("data.json")
+
+ # New functions
+    @app.route("/about/")
+    def about():
+        return render_template("about.html")
+
+    @app.route("/contact/")
+    def contact():
+        return render_template("contact.html")
+
+# Allows you to click RUN button
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5421)
